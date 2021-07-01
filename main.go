b6fa48c86a02c7978f731a97def8c0fcfc2ed2a4
Joey Brayshaw <joeybrayshaw@gmail.com>
2021-07-01 14:30:18 +0100

    changed datetime format

--- main.go
+++ main.go
@@ -23,6 +23,8 @@ func main() {
 	for {
 		wait()
 		t := time.Now()
+
+		// There's probably a tidier way to do this!
 		zone, _ := t.Zone()
 		fmt.Printf("%v-%v-%v %v:%v:%v %v - %v\n", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), zone, LOGSTRING)
 	}
