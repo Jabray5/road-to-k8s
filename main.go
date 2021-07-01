fc2ccdaa42ca03d8493fab05f599d2b0c97ccf53
Joey Brayshaw <joeybrayshaw@gmail.com>
2021-07-01 11:10:21 +0100

    Loop and log implemented

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+const SLEEPTIME int = 10
+const LOGSTRING string = "A random string of your choice."
+
+func main() {
+
+	// Get the log file, or create one if it doesn't exist
+	file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+
+	// Loop indefinitely
+	for {
+		wait()
+		log.Println(LOGSTRING)
+		fmt.Println("Log added.")
+	}
+}
+
+func wait() {
+	time.Sleep(time.Duration(SLEEPTIME) * time.Second)
+}
