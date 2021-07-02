416ece5f1b8a26dc893e60bb13f5304cfe004df2
Joey Brayshaw <joeybrayshaw@gmail.com>
2021-07-02 14:43:47 +0100

    starting api

---
+++ api/api.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homepage)
+	log.Fatal(http.ListenAndServe("8081", nil))
+}
+
+func StartListening() {
+	handleRequests()
+}
--- main.go
+++ main.go
@@ -3,24 +3,39 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"./api"
 )
 
 const SLEEPTIME int = 10
 const LOGSTRING string = "A random string!"
 
+var enableLogging bool = true
+
 func main() {
 
 	// Loop indefinitely
 	for {
-		wait()
-		t := time.Now()
+		if enableLogging {
+			logTime()
+			wait()
+		}
+		api.StartListening()
 
-		// There's probably a tidier way to do this!
-		zone, _ := t.Zone()
-		fmt.Printf("%v-%v-%v %v:%v:%v %v - %v\n", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), zone, LOGSTRING)
+		// if newMessage {
+		// 	// log the new message
+		// }
+
 	}
 }
 
 func wait() {
 	time.Sleep(time.Duration(SLEEPTIME) * time.Second)
+}
+
+func logTime() {
+	t := time.Now()
+	// There's probably a tidier way to do this!
+	zone, _ := t.Zone()
+	fmt.Printf("%v-%v-%v %v:%v:%v %v - %v\n", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), zone, LOGSTRING)
 }
