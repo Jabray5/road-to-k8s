0a50f179adc6a61e1d14c2296b75fd21e6e79fce
Joey Brayshaw <joeybrayshaw@gmail.com>
2021-07-02 11:19:38 +0100

    changed default sleeptime to 10s

--- main.go
+++ main.go
@@ -5,19 +5,10 @@ import (
 	"time"
 )
 
-const SLEEPTIME int = 3
+const SLEEPTIME int = 10
 const LOGSTRING string = "A random string!"
 
 func main() {
-
-	// FOR CREATING A LOG.TXT FILE
-	// Get the log file, or create one if it doesn't exist
-	// file, err := os.OpenFile("./log.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-	// if err != nil {
-	// 	fmt.Println("Error", err)
-	// 	log.Fatal(err)
-	// }
-	// log.SetOutput(file)
 
 	// Loop indefinitely
 	for {
